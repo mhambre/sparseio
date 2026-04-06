9c07be9d89e5e06d7933e1b5d793bb4f4aa97cda
Matthew Hambrecht <hambrechtmatt@gmail.com>
2026-04-06 11:17:58 -0400

    docustring cleanup

--- src/sources/mod.rs
+++ src/sources/mod.rs
@@ -1,4 +1,4 @@
-//! Example source implementations. Quick and dirty implementations of the [`crate::Reader`], and [`crate::Writer`] for
+//! Quick and dirty implementations of the [`crate::Reader`], and [`crate::Writer`] for
 //! dealing with common use cases (File -> Sparse Memory, File -> Sparse File, HTTP Range Reader -> Sparse File).
 //!
 //! While these can be used as-is, they are primarily intended as examples and starting points for building your own custom sources.
