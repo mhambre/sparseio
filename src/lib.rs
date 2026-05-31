0919f1e1ddbe8e6cbc0563879e502f337ec7bef3
Matthew Hambrecht <hambrechtmatt@gmail.com>
2026-05-30 23:14:35 -0400

    CI cleanup + compile error

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+compile_error!(
+    r"
+This crate is currently undergoing an architectural refactor. Please stay patient
+and keep track of http://github.com/mhambre/sparseio for development updates.
+"
+);
